0bffc673199f27f02fcdd4d07de32cc270160556
Kevin Lee <leekevin@berkeley.edu>
2018-03-31 14:43:17 -0700

    Betting on the basics

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import 'expo';
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Keyboard, Picker, PickerIOS, StyleSheet, TextInput, Text,
-  TouchableWithoutFeedback, View
+  Button, Keyboard, Picker, PickerIOS, StyleSheet, TextInput, Text,
+  TouchableWithoutFeedback, TouchableOpacity, View
 } from 'react-native';
+
+import Modal from "react-native-modal";
 
 const ALLOWABLE_MONTHS = 12;
 const ALLOWABLE_DAYS = 366;
@@ -27,15 +29,16 @@ function validateInvestmentMagnitude(instance) {
   }
 }
 
-export default class App extends React.Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       amountToInvest: '',
       amountDesired: '',
       investmentPeriodMagnitude: '',
-      investmentPeriodUnits: '',
-      hasValidInvestmentMagnitude: true
+      investmentPeriodUnits: 'days',
+      hasValidInvestmentMagnitude: true,
+      isUnitModalOpen: false
     }
   }
   checkAndWarnInvestmentMagnitude() {
@@ -46,43 +49,76 @@ export default class App extends React.Component {
       hasValidInvestmentMagnitude: validateInvestmentMagnitude(this)
     });
   }
+  openUnitPicker() {
+    console.log('Open modal');
+    this.setState({
+      isUnitModalOpen: !this.state.isUnitModalOpen
+    });
+  }
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View style={styles.container}>
-          <Text>What is the amount that you want to invest?</Text>
-          <TextInput
-            style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
-            multiline={false} keyboardType="numeric"
-            onChangeText={(text) => this.setState({amountToInvest: text})}
-            value={this.state.amountToInvest}
-          />
-          <Text>What is the amount that you want to have?</Text>
-          <TextInput
-            style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
-            multiline={false} keyboardType="numeric"
-            onChangeText={(text) => this.setState({amountDesired: text})}
-            value={this.state.amountDesired}
-          />
-          {/* <View style={{flex: 1, flexDirection: 'row'}}> */}
-            <Text>How long are you willing to invest?</Text>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <View style={styles.container}>
+            <Text style={{fontSize: 36, color: '#008000' }}>BE$T BET</Text>
+            <Text> What is the amount that you want to invest?</Text>
             <TextInput
-              style={{height: 40, width: 100, borderColor: this.state.hasValidInvestmentMagnitude ? 'gray' : 'red' , borderWidth: 1}}
+              style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1,
+                      paddingBottom: 10}}
               multiline={false} keyboardType="numeric"
-              onChangeText={(text) => this.setState({investmentPeriodMagnitude: text})}
-              value={this.state.investmentPeriodMagnitude}
+              onChangeText={(text) => this.setState({amountToInvest: text})}
+              value={this.state.amountToInvest}
             />
-            <Picker
-              style={{ width: 100 }} mode="dropdown"
-              selectedValue={this.state.investmentPeriodUnits}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({investmentPeriodUnits: itemValue},
-                              this.checkAndWarnInvestmentMagnitude)}>
-              <Picker.Item key="days" label="days" value="days" />
-              <Picker.Item key="weeks" label="weeks" value="weeks" />
-              <Picker.Item key="months" label="months" value="months" />
-            </Picker>
-          {/* </View> */}
+            <Text>What is the amount that you want to have?</Text>
+            <TextInput
+              style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1,
+                      paddingBottom: 10}}
+              multiline={false} keyboardType="numeric"
+              onChangeText={(text) => this.setState({amountDesired: text})}
+              value={this.state.amountDesired}
+            />
+            <Text>How long are you willing to invest?</Text>
+            <View style={{height: 40, flexDirection: 'row', alignContent: 'center', justifyContent: 'center'}}>
+              <TextInput
+                style={{
+                  flex: 1, height: 40, borderWidth: 1, maxWidth: 100,
+                  borderColor: this.state.hasValidInvestmentMagnitude ? 'gray' : 'red'}}
+                multiline={false} keyboardType="numeric"
+                onChangeText={(text) => this.setState({investmentPeriodMagnitude: text})}
+                value={this.state.investmentPeriodMagnitude}
+              />
+              <Text style={{flex: 1, height: 40, paddingTop: 10, paddingLeft: 10, maxWidth: 100}}
+                    onPress={this.openUnitPicker.bind(this)}>
+                {this.state.investmentPeriodUnits}
+              </Text>
+            </View>
+            <Modal isVisible={this.state.isUnitModalOpen}
+                   style={{backgroundColor: 'white', paddingLeft: 22, paddingRight: 22,
+                           paddingTop: 0, paddingBottom: 0,
+                           maxHeight: 250, marginTop: 'auto',
+                           justifyContent: "center", alignItems: "center",
+                           borderRadius: 4, borderColor: "rgba(0, 0, 0, 0.1)"}}
+                   onBackdropPress={() => this.setState({ isUnitModalOpen: false })}>
+              <View>
+                <Picker
+                  style={{width: 100, bottom: 0}}
+                  selectedValue={this.state.investmentPeriodUnits}
+                  onValueChange={(itemValue, itemIndex) =>
+                    this.setState({investmentPeriodUnits: itemValue},
+                                  this.checkAndWarnInvestmentMagnitude)}>
+                  <Picker.Item key="days" label="days" value="days" />
+                  <Picker.Item key="weeks" label="weeks" value="weeks" />
+                  <Picker.Item key="months" label="months" value="months" />
+                </Picker>
+                <TouchableOpacity onPress={this.openUnitPicker.bind(this)}>
+                  <Text style={{
+                    fontSize: 12, marginLeft: 'auto', marginRight: 'auto', marginBottom: 10}}>
+                    Close
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </Modal>
+          </View>
         </View>
       </TouchableWithoutFeedback>
     );
@@ -92,9 +128,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 100,
+    paddingTop: 140,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
