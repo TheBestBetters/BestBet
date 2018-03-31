b4f6d0cf6d876cc136ab6552eceb54bac9d856af
Kevin Lee <leekevin@berkeley.edu>
2018-03-31 09:05:54 -0700

    Making sure that all bets are valid

--- App.js
+++ App.js
@@ -1,33 +1,90 @@
 import 'expo';
 import React from 'react';
-import {Picker, PickerIOS, StyleSheet, TextInput, Text, View} from 'react-native';
+import {
+  Keyboard, Picker, PickerIOS, StyleSheet, TextInput, Text,
+  TouchableWithoutFeedback, View
+} from 'react-native';
+
+const ALLOWABLE_MONTHS = 12;
+const ALLOWABLE_DAYS = 366;
+const ALLOWABLE_WEEKS = 52;
+
+function validateInvestmentMagnitude(instance) {
+  var investmentPeriodUnits = instance.state.investmentPeriodUnits;
+  var investmentPeriodMagnitude = parseInt(
+    instance.state.investmentPeriodMagnitude);
+
+  if (investmentPeriodMagnitude == NaN) {
+    return false;
+  }
+
+  if (investmentPeriodUnits == 'days') {
+    return investmentPeriodMagnitude <= 366;
+  } else if (investmentPeriodUnits == 'weeks') {
+    return investmentPeriodMagnitude <= 52;
+  } else {
+    return investmentPeriodMagnitude <= 12;
+  }
+}
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       amountToInvest: '',
-      investmentPeriod: ''
+      amountDesired: '',
+      investmentPeriodMagnitude: '',
+      investmentPeriodUnits: '',
+      hasValidInvestmentMagnitude: true
     }
+  }
+  checkAndWarnInvestmentMagnitude() {
+    if (this.state.investmentPeriodMagnitude == '') {
+      return;
+    }
+    this.setState({
+      hasValidInvestmentMagnitude: validateInvestmentMagnitude(this)
+    });
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.state.amountToInvest}</Text>
-        <TextInput
-          style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(text) => this.setState({amountToInvest: text})}
-          value={this.state.amountToInvest}
-        />
-        <Text>Investment Period: {this.state.investmentPeriod}</Text>
-        <Picker
-          style={{height:30, width:100}}
-          selectedValue={this.state.investmentPeriod}
-          onValueChange={(itemValue, itemIndex) => this.setState({investmentPeriod: itemValue})}>
-          <Picker.Item key="java" label="Java" value="java" />
-          <Picker.Item key="js" label="JavaScript" value="js" />
-        </Picker>
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={styles.container}>
+          <Text>What is the amount that you want to invest?</Text>
+          <TextInput
+            style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
+            multiline={false} keyboardType="numeric"
+            onChangeText={(text) => this.setState({amountToInvest: text})}
+            value={this.state.amountToInvest}
+          />
+          <Text>What is the amount that you want to have?</Text>
+          <TextInput
+            style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
+            multiline={false} keyboardType="numeric"
+            onChangeText={(text) => this.setState({amountDesired: text})}
+            value={this.state.amountDesired}
+          />
+          {/* <View style={{flex: 1, flexDirection: 'row'}}> */}
+            <Text>How long are you willing to invest?</Text>
+            <TextInput
+              style={{height: 40, width: 100, borderColor: this.state.hasValidInvestmentMagnitude ? 'gray' : 'red' , borderWidth: 1}}
+              multiline={false} keyboardType="numeric"
+              onChangeText={(text) => this.setState({investmentPeriodMagnitude: text})}
+              value={this.state.investmentPeriodMagnitude}
+            />
+            <Picker
+              style={{ width: 100 }} mode="dropdown"
+              selectedValue={this.state.investmentPeriodUnits}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({investmentPeriodUnits: itemValue},
+                              this.checkAndWarnInvestmentMagnitude)}>
+              <Picker.Item key="days" label="days" value="days" />
+              <Picker.Item key="weeks" label="weeks" value="weeks" />
+              <Picker.Item key="months" label="months" value="months" />
+            </Picker>
+          {/* </View> */}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
