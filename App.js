752c5aeb84aa36dad14cf66669c79149629514ac
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2018-04-04 18:52:17 -0700

    fetch request and more

--- App.js
+++ App.js
@@ -284,7 +284,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 100,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'red',
     alignItems: 'center',
   },
 
